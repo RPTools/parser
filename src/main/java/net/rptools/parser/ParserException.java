b12a4f73d453ef33c6e3f1b73c2bd93271e86a7b
Merudo <filteau.guillaume@gmail.com>
2020-05-17 03:38:36 -0500

    Add macroStackTrace to the parser
    - Add methods addMacro and getMacroStackTrace to ParserException to add to, and return, the macro stack trace associated with the error
    - Part of RPTools/maptool#1861

--- src/main/java/net/rptools/parser/ParserException.java
+++ src/main/java/net/rptools/parser/ParserException.java
@@ -14,9 +14,15 @@
  */
 package net.rptools.parser;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class ParserException extends Exception {
 
   private static final long serialVersionUID = 1959865440126054220L;
+
+  /** The list of macro calls that resulted in the error. */
+  private final List<String> macroStackTrace = new ArrayList<>();
 
   public ParserException(Throwable cause) {
     super(cause);
@@ -24,5 +30,19 @@ public class ParserException extends Exception {
 
   public ParserException(String msg) {
     super(msg);
+  }
+
+  /**
+   * Add the macro / udf name to the stackTrace.
+   *
+   * @param name the macro or UDF name
+   */
+  public void addMacro(String name) {
+    macroStackTrace.add(name);
+  }
+
+  /** @return an array representing the macro stack trace. */
+  public String[] getMacroStackTrace() {
+    return macroStackTrace.toArray(new String[0]);
   }
 }
